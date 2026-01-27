f716de7a1e9c406ea4284731926a5c9808beb817
danimb14 <daniel.moore04@gmail.com>
2026-01-27 11:08:04 +0100

    Endpoint GET /me

--- backend/src/routes/me.routes.js
+++ backend/src/routes/me.routes.js
@@ -6,8 +6,9 @@ const router = express.Router();
 router.get("/me", requireAuth, (req, res) => {
   // req.user ve del middleware requireAuth
   res.json({
-    ok: true,
-    user: req.user,
+    id: req.user.id,
+    email: req.user.email,
+    role: req.user.role,
   });
 });
 
